ab5cf83823e1447dd817e03fb01672181696e53a
marleypm16 <yarlempm16@gmail.com>
2025-01-19 20:58:30 -0300

    authorization

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import express, {Express} from "express";
 import { userRouter } from "./userRouter";
 import { authRouter } from "./authRoute";
+import { authorizationRoute } from "./authorizationRoute";
 
 const router = (app : Express) =>{
     app.use(
         express.json(),
+        authorizationRoute,
         authRouter,
         userRouter
     )
--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,5 +1,6 @@
 import db from "../../db/db";
 import { sign } from "jsonwebtoken";
+import bcrypt from "bcrypt";
 export class AuthService {
     async login(email:string) {
         const user = await db.user.findUnique({
@@ -10,6 +11,8 @@ export class AuthService {
         if (!user) {
             throw new Error('User not found')
         }
+        senhasIguais = bcrypt.compare(user.password,p)
+
         const token = sign({email}, process.env.JWT_SECRET as string,
             {
                 expiresIn:8699
